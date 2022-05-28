7cece7a4bfde77a22fbb778120993e1f6bc9911f
luizfelipetorres <fellipe116@gmail.com>
2022-05-28 20:59:21 -0300

    Adicionado middlewares

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ const server = express();
 // faz com que o express use JSON
 server.use(express.json()); 
 
+function verificarNomeSobrenome(req, res, next) {
+    if (!req.body.name || !req.body.lastname) {
+    return res.status(400).json({ error: 'Insira nome e sobrenome!' });
+    // middleware local que irá checar se as propriedades name e lastname 
+    //foram informadas corretamente,
+    // caso negativo, irá retornar um erro 400 – BAD REQUEST
+    }
+    return next(); // se o nome for informado corretamente, a função next() chama as próximas ações
+    }
+
+// Checa se o index já foi cadastrado
+function verificarIndice(req, res, next) {
+    const pessoa = pessoas[req.params.index];
+    if (!pessoa) {
+        return res.status(400).json({ error: 'Pessoa não cadastrada' });
+    } 
+    
+    req.pessoa = pessoa;
+    return next();
+}
+
 //Criando um array com uma pessoa inicialmente
 const pessoas = [{
     "name": "Luiz",
@@ -19,19 +40,19 @@ server.get('/pessoas', (req, res) => {
 })
 
 //Retorna apenas a pessoa no index  
-server.get('/pessoas/:index', (req, res) => {
+server.get('/pessoas/:index', verificarIndice, (req, res) => {
     const { index } = req.params;
     return res.json(pessoas[index]);
 }) 
 
 //Cadastrar uma nova pessoa
-server.post('/pessoas', (req, res) => {
+server.post('/pessoas', verificarNomeSobrenome, (req, res) => {
     pessoas.push(req.body);
     return res.json(pessoas); // retorna a informação da variável pessoas
 }) 
 
 //Atualizar uma pessoa existente
-server.put('/pessoas/:index', (req, res) => {
+server.put('/pessoas/:index', verificarIndice, verificarNomeSobrenome, (req, res) => {
 
     //Armazenar indice em constante
     const { index } = req.params;   
